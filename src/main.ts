fed09c8069eb3862b2e03a4f61ce9abb62869c53
Saif-Haq <saifhaq01@gmail.com>
2024-10-03 11:40:54 +0500

    :)

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS with specific options
+  // Enable CORS with specific options :)
   app.enableCors({
     origin: ['https://studio.apollographql.com', 'http://localhost:5173', 'http://localhost:5174'], // Allow requests from Apollo Studio
     credentials: true,  // Enable sending cookies or authorization headers with requests
